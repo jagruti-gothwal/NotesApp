bba71e0ac0481f4b953343ce7afea543f52b427d
jagruti-gothwal <gothwaljagruti@gmail.com>
2021-09-30 16:10:54 +0530

    Conflicts resolved

--- public/js/notes.js
+++ public/js/notes.js
@@ -1,28 +1,40 @@
-let tableBody = document.getElementById('tableBody');
-function update(){
-    let str ="";
-    if (localStorage.getItem('itemsJson')!=null) {
-    itemsJson = JSON.parse(localStorage.getItem('itemsJson'));
-   
+let tableBody = document.getElementById("tableBody");
+let clearStorage = document.getElementById("clearStorage");
+function update() {
+  let str = "";
+  if (localStorage.getItem("itemsJson") != null) {
+    itemsJson = JSON.parse(localStorage.getItem("itemsJson"));
+
     itemsJson.forEach((element, index) => {
-    str+= `
+      str += `
     <tr>
-    <th scope="row">${index+1}</th>
+    <th scope="row">${index + 1}</th>
     <td>${element[0]}</td>
     <td>${element[1]}</td>
-    <td><button id="dBtn" onclick="deleted(${index});">Delete</button>  <button id="eBtn">Edit</button></td>
+    <td><button id="dBtn" onclick="deleted(${index});">Delete</button> </td>
     </tr>`;
-});
-}
-tableBody.innerHTML = str;
+    });
+  }
+  tableBody.innerHTML = str;
 }
 update();
 
-function deleted(itemIndex){
-    console.log("button clicked",itemIndex);
-    itemsJson = JSON.parse(localStorage.getItem('itemsJson'));
-    //Delete 
-    itemsJson.splice(itemIndex,1)
-    localStorage.setItem('itemsJson', JSON.stringify(itemsJson))
-    update();
+function deleted(itemIndex) {
+  console.log("button clicked", itemIndex);
+  itemsJson = JSON.parse(localStorage.getItem("itemsJson"));
+  //Delete
+  itemsJson.splice(itemIndex, 1);
+  localStorage.setItem("itemsJson", JSON.stringify(itemsJson));
+  update();
 }
+
+clearStorage.addEventListener("click", () => {
+  if (confirm("Are you sure you want to delete this note!")) {
+    console.log("yes");
+    // TODO: Create a form and use post request to submit a form
+    localStorage.clear();
+  } else {
+    console.log("no");
+  }
+  update();
+});
