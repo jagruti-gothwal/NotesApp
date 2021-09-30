ca8c0c1233b4a8e823b3e0714f865cfaa381dcaa
jagruti-gothwal <gothwaljagruti@gmail.com>
2021-09-30 16:13:41 +0530

    Conflicts resolved

--- src/app.js
+++ src/app.js
@@ -1,54 +1,51 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 7000;
-const hbs = require('hbs');
+const hbs = require("hbs");
 
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
 
-
-app.set('view engine', 'hbs')
-app.set('views', template_path);
+app.set("view engine", "hbs");
+app.set("views", template_path);
 hbs.registerPartials(partials_path);
 
 app.use(express.static(static_path));
 
 app.get("", (req, res) => {
-    res.render('index')
-})
+  res.render("index");
+});
 
 app.get("/login", (req, res) => {
-    res.render('login')
-})
+  res.render("login");
+});
 
 app.get("/register", (req, res) => {
-    res.render('register')
-})
+  res.render("register");
+});
 
 app.post("/notes", (req, res) => {
-    res.render('notes')
-})
-
+  res.render("notes");
+});
 
 app.get("/notes", (req, res) => {
-    res.render('notes')
-})
+  res.render("notes");
+});
 
 app.get("/game", (req, res) => {
-    res.render('game')
-})
+  res.render("game");
+});
 
 app.get("/contact", (req, res) => {
-    res.render('contact')
-})
+  res.render("contact");
+});
 
 app.get("/error", (req, res) => {
-    res.send('error')
-})
-
+  res.send("error");
+});
 
 app.listen(port, () => {
-    console.log(`Server is listening at ${port}`);
-})
+  console.log(`Server is listening at ${port}`);
+});
